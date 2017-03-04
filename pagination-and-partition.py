9ab7255fbc85bb4898a00c9bfc6e7380bf928c76
Niels Lerche Sørensen <nielslerches@gmail.com>
2017-03-04 01:19:40 +0100

    Update pagination-and-partition.py

--- pagination-and-partition.py
+++ pagination-and-partition.py
@@ -1,8 +1,9 @@
-# Parts a flat array into an array of chunk of sizes page_size.
+# Parts a flat array into an array of of arrays, each with specified size. Includes the last inner array, even if not to size.
 def paginate(arr, page_size=3):
-	pages = []
+	result = []
 	start = 0
 	end = 0
+	
 	for index, value in enumerate(arr):
 		end = index+page_size
 		if index % page_size == 0:
@@ -11,6 +12,18 @@ def paginate(arr, page_size=3):
 			start = end
 	return pages
 
-# Parts a flat array into an array of chunks, each with size n_parts
+# Parts a flat array into an array of of arrays, where the size is calculated based on the number of inner arrays.
 def partition(arr, n_parts=2):
 	return paginate(arr, int(len(arr)/n_parts))
+
+# Quick partitioning (~ ½ dt of partition(). Parts a flat array into an array of arrays, each with specified size. Doesn't include the last inner array, if not to size.
+def q_partition(arr, size=3):
+    result = []
+    start = 0
+    end = 0
+    
+    for i in range(int(len(arr)/size)):
+        end = start + size
+        result.append(arr[start:end])
+        start = end
+    return result
